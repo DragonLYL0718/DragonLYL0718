e883688e1fa6399b13b709731b292931ee5ff974
lylrayleigh <528539124@qq.com>
2024-02-04 14:49:54 +0800

    Solve figure reopen issue

--- assets/js/figure.js
+++ assets/js/figure.js
@@ -1,27 +1,40 @@
-// 创建模态窗口和图片元素
-var modal = document.createElement('div');
-modal.classList.add('modal');
-modal.innerHTML = '<span class="close">&times;</span><img class="modal-content" id="modal-image">';
-document.body.appendChild(modal);
-modal.style.display = "none";
-// 获取模态窗口元素和关闭按钮
-var modalImg = document.getElementById("modal-image");
-var closeBtn = document.querySelector('.modal .close');
+function initModalFunctionality() {
 
-// 函数：打开模态窗口
-function openModal(imageSrc) {
-    modal.style.display = "block";
-    modalImg.src = imageSrc;
+    var modal = document.querySelector('.modal');
+    if (!modal) {
+        
+        var modal = document.createElement('div');
+        modal.classList.add('modal');
+        modal.innerHTML = '<span class="close">&times;</span><img class="modal-content" id="modal-image">';
+        document.body.appendChild(modal);
+        modal.style.display = "none";
+        console.log('Modal functionality initialized.');
+    }
+    
+    var modalImg = document.getElementById("modal-image");
+    var closeBtn = document.querySelector('.modal .close');
+
+    function openModal(imageSrc) {
+        modal.style.display = "block";
+        modalImg.src = imageSrc;
+    }
+
+    document.querySelectorAll('.image-link').forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            openModal(this.href);
+        });
+    });
+
+    closeBtn.addEventListener('click', function() {
+        modal.style.display = "none";
+    });
+    console.log('Modal functionality Upadated.');
 }
 
-// 为每个图片链接添加点击事件监听器
-document.querySelectorAll('.image-link').forEach(function(link) {
-    link.addEventListener('click', function(event) {
-    event.preventDefault(); // 阻止链接默认打开新标签页的行为
-    openModal(this.href); // 传递原图链接到打开模态窗口的函数
-    });
-});
-// 点击关闭按钮隐藏模态窗口
-closeBtn.addEventListener('click', function() {
-    modal.style.display = "none";
-});
+// Ensure that the initModalFunctionality function is called after the DOM is fully loaded.
+if (document.readyState === 'complete') {
+    initModalFunctionality();
+  } else {
+    document.addEventListener('DOMContentLoaded', initModalFunctionality);
+}
