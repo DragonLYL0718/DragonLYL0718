c4bcdc6f60120db2fee5177cb548056956980786
lylrayleigh <57632904+DragonLYL0718@users.noreply.github.com>
2025-04-22 19:08:26 +0800

    Update

--- assets/js/gallery.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("gallery.js is loaded");
-    document.querySelectorAll('.gallery-link').forEach(function(link) {
-        link.addEventListener('click', function(event) {
-        setTimeout(() => window.location.reload(), 200)
-        });
-    });
-});
